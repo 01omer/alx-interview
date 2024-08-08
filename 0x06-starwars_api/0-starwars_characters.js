f3cb19834d890d54739cb4fb81486d28209a152f
01omer <omer00091@outlook.com>
2024-08-08 22:59:19 +0000

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,20 @@
 #!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-const request = require('request');
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-// Check if a movie ID is provided
-if (process.argv.length < 3) {
-  console.log('Usage: ./script.js <movie_id>');
-  process.exit(1);
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
-const movieId = process.argv[2];
-
-// Fetch the movie details
-request(
-  'https://swapi.dev/api/films/' + movieId + '/',
-  function (err, res, body) {
-    if (err) throw err;
-
-    // Parse the response body
-    const actors = JSON.parse(body).characters;
-
-    // Function to print characters in exact order
-    const exactOrder = (actors, index) => {
-      if (index === actors.length) return;
-
-      // Fetch each character details
-      request(actors[index], function (err, res, body) {
-        if (err) throw err;
-
-        // Print the character name
-        console.log(JSON.parse(body).name);
-
-        // Recursive call to process the next character
-        exactOrder(actors, index + 1);
-      });
-    };
-
-    // Start the process with the first character
-    exactOrder(actors, 0);
-  }
-);
+starwarsCharacters(filmID);
